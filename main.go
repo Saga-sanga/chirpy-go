57dc49c5113a46e85f26f311bc15c5ed068fabf4
Saga-sanga <vulcan248@gmail.com>
2025-08-25 07:27:53 +0000

    feat: add server with mux

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "net/http"
 
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+	server.ListenAndServe()
 }
