d8e672161cb73ca11cd50072772056911c43072a
Saga-sanga <vulcan248@gmail.com>
2025-09-02 08:46:44 +0000

    refactor: create json response and error methods

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -10,18 +9,6 @@ import (
 type apiConfig struct {
 	// atomic.Int32 helps us safely increment and read across multiple goroutines
 	fileserverHits atomic.Int32
-}
-
-type Chirp struct {
-	Body string `json:"body"`
-}
-
-type ChirpError struct {
-	Error string `json:"error"`
-}
-
-type ChirpValid struct {
-	Valid bool `json:"valid"`
 }
 
 func main() {
@@ -36,10 +23,12 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(fsHandler)))
+
 	mux.HandleFunc("GET /api/healthz", healthzHandler)
+	mux.HandleFunc("POST /api/validate_chirp", handleValidateChirp)
+
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handleValidateChirp)
 
 	srv := &http.Server{
 		Handler: mux,
@@ -48,34 +37,4 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
-}
-
-func (cfg *apiConfig) handleValidateChirp(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	chirp := Chirp{}
-	if err := decoder.Decode(&chirp); err != nil {
-		log.Printf("Error decoding chirp: %s", err)
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(500)
-		w.Write([]byte(`{"error": "Something went wrong"}`))
-		return
-	}
-	if len(chirp.Body) > 140 {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"error": "Chirp is too long"}`))
-		return
-	}
-	respBody := ChirpValid{
-		Valid: true,
-	}
-	dat, err := json.Marshal(respBody)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(dat)
 }
