39237a75e95b099a2265afcd9afa17e2bd75ab17
Saga-sanga <vulcan248@gmail.com>
2025-09-02 08:31:55 +0000

    feat: add handler for chirp validation

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -9,6 +10,18 @@ import (
 type apiConfig struct {
 	// atomic.Int32 helps us safely increment and read across multiple goroutines
 	fileserverHits atomic.Int32
+}
+
+type Chirp struct {
+	Body string `json:"body"`
+}
+
+type ChirpError struct {
+	Error string `json:"error"`
+}
+
+type ChirpValid struct {
+	Valid bool `json:"valid"`
 }
 
 func main() {
@@ -26,6 +39,7 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", healthzHandler)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /api/validate_chirp", apiCfg.handleValidateChirp)
 
 	srv := &http.Server{
 		Handler: mux,
@@ -34,4 +48,34 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func (cfg *apiConfig) handleValidateChirp(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+	chirp := Chirp{}
+	if err := decoder.Decode(&chirp); err != nil {
+		log.Printf("Error decoding chirp: %s", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		w.Write([]byte(`{"error": "Something went wrong"}`))
+		return
+	}
+	if len(chirp.Body) > 140 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "Chirp is too long"}`))
+		return
+	}
+	respBody := ChirpValid{
+		Valid: true,
+	}
+	dat, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(dat)
 }
