e93c14561efe87aae0ac3fc2c7075db40b59543b
Saga-sanga <vulcan248@gmail.com>
2025-09-04 09:12:12 +0000

    initiate db connection in main

--- main.go
+++ main.go
@@ -1,24 +1,40 @@
 package main
 
-import _ "github.com/lib/pq"
-
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/saga-sanga/chirpy-go/internal/database"
 )
 
 type apiConfig struct {
 	// atomic.Int32 helps us safely increment and read across multiple goroutines
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+
+	dbQueries := database.New(db)
+
 	const filepathRoot = "."
 	const port = "8080"
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		dbQueries:      dbQueries,
 	}
 
 	fsHandler := http.FileServer(http.Dir(filepathRoot))
