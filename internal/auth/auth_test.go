e6d8fa6060919a8c781ebbff5b8991d131b8ed93
Saga-sanga <vulcan248@gmail.com>
2025-09-16 09:37:41 +0000

    refactor: auth test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,18 +2,58 @@ package auth
 
 import (
 	"testing"
-
-	"golang.org/x/crypto/bcrypt"
 )
 
 func TestCheckPasswordHash(t *testing.T) {
-	password := "hellokitty123"
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		t.Errorf("Error hashing: %s", err)
+	password1 := "correctPassword123!"
+	password2 := "anotherPassword1234@"
+	hash1, _ := HashPassword(password1)
+	hash2, _ := HashPassword(password2)
+
+	tests := []struct {
+		name     string
+		password string
+		hash     string
+		wantErr  bool
+	}{
+		{
+			name:     "Correct password",
+			password: password1,
+			hash:     hash1,
+			wantErr:  false,
+		},
+		{
+			name:     "Incorrect password",
+			password: "WrongPassword",
+			hash:     hash1,
+			wantErr:  true,
+		},
+		{
+			name:     "Password doesn't match different hash",
+			password: password1,
+			hash:     hash2,
+			wantErr:  true,
+		},
+		{
+			name:     "Empty password",
+			password: "",
+			hash:     hash1,
+			wantErr:  true,
+		},
+		{
+			name:     "Invalid hash",
+			password: password1,
+			hash:     "invalidhash",
+			wantErr:  true,
+		},
 	}
 
-	if err := CheckPasswordHash(password, string(hash)); err != nil {
-		t.Errorf("Hash failed: %s", err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := CheckPasswordHash(tt.password, tt.hash)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CheckPasswordHash() error = %v, wantErr = %v", err, tt.wantErr)
+			}
+		})
 	}
 }
